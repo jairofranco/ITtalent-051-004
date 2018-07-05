735aa60dfe99e9d910d30ea32b9e5b86f1cb808b
jqgekko <jqgekko@gmail.com>
2018-07-05 15:58:58 -0500

    agregando llave foranea

--- basededatos/residuo.sql
+++ basededatos/residuo.sql
@@ -2,7 +2,7 @@ CREATE TABLE residuo
 (
 residuo_id SERIAL PRIMARY KEY,
 	nombre VARCHAR NOT NULL,
-	material_id INTEGER NOT NULL,
+	material_id INTEGER NOT NULL,/* llave foranea*/
 	descripcion TEXT,
 	foto BYTEA /*para guardar archivos de imagen*/
 );

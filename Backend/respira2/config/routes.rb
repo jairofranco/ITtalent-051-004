1153217cc24ca6537ec3bbaced2456014cc3301a
sigmotoa <sigmotoa@misena.edu.co>
2018-07-12 15:46:25 -0500

    Controlador/Apuntado!

--- Backend/respira2/config/routes.rb
+++ Backend/respira2/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => "Welcome#index"
+  root :to => "welcome#index"
 end
